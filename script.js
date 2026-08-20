4bc3da297b272d066e8c6eb46fb0d90beaa1ac65
Palhares1709 <fcpalharess@gmail.com>
2026-08-20 08:44:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+// HEADER SCROLL
+window.addEventListener("scroll", () => {
+    document.getElementById("header")
+    .classList.toggle("scrolled", window.scrollY > 50);
+});
 
+// ANIMAÇÃO
+const elements = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+        }
+    });
+});
+
+elements.forEach(el => observer.observe(el));
+
+// FORM → WHATSAPP
+document.getElementById("formContato").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    let nome = this.querySelector("input[type=text]").value;
+    let msg = this.querySelector("textarea").value;
+
+    let texto = `Olá, meu nome é ${nome}. ${msg}`;
+
+    let url = `https://wa.me/5599999999999?text=${encodeURIComponent(texto)}`;
+
+    window.open(url, "_blank");
+});
